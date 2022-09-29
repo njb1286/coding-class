3aca72b4211c68ea9f77818e68109f22a089f6e4
Njb1286 <bennionnj@gmail.com>
2022-09-29 10:50:10 -0600

    Added contentVisible to the form and togglability to the generate button

--- src/components/apps/mad-libs/card.js
+++ src/components/apps/mad-libs/card.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 
 import Input from './input';
-import { removeCapital } from './helpers';
+import { parseCamelCase } from './helpers';
+import Content from './content';
 
 export default class Card extends Component {
     constructor(props) {
@@ -23,46 +24,65 @@ export default class Card extends Component {
             adjectiveFour: "",
             nounThree: "",
             celebrityFour: "",
-            adjectiveFive: ""
+            adjectiveFive: "",
+            contentVisible: false
         }
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleGetState = this.handleGetState.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleInputChange(event) {
         this.setState({ [event.target.name]: event.target.value });
-        console.log(this.state);
     }
 
     handleGetState(key) {
         return this.state[key]
     }
 
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log("Trying to submit");
+        this.setState({
+            contentVisible: !this.state.contentVisible // I should have thought of this a long time ago! Being able to set the value to not the current value! (Swapping)
+        })
+    }
+
     render() {
 
-        const inputData = Object.keys(this.state).map(item => {
+        const blackList = [
+            "contentVisible"
+        ]
+
+        const inputData = Object.keys(this.state).filter(item => !blackList.includes(item)).map(item => {
             return {
-                title: removeCapital(item),
+                title: parseCamelCase(item),
                 name: item
             }
         })
 
         return (
             <div className='card'>
-                <h1>{this.state.color}</h1>
+                <form onSubmit={this.handleSubmit} className="card__inputs">
+                    {
+                        inputData.map(item => (
+                            <Input
+                                key={item.name} 
+                                title={item.title} 
+                                state={item.name} 
+                                handleGetState={this.handleGetState} 
+                                handleInputChange={this.handleInputChange} 
+                            />
+                        ))
+                    }
+                    {/* <button type='subit'>{!this.state.contentVisible ? "Generate Mad lib" : "Clear Form"}</button> */}
+                    <input type="submit" value={!this.state.contentVisible ? "Generate Mad Lib" : "Clear Form"} className={!this.state.contentVisible ? "generate-btn" : "clear-btn"} />
+                    {
+                        this.state.contentVisible ? <Content data={this.state} /> : null
+                    }
+                </form>
 
-                {
-                    inputData.map(item => (
-                        <Input
-                            key={item.name} 
-                            title={item.title} 
-                            state={item.name} 
-                            handleGetState={this.handleGetState} 
-                            handleInputChange={this.handleInputChange} 
-                        />
-                    ))
-                }
             </div>
         )
     }
