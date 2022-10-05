b89ac3f97aa2654b4886c81ca3df6fac51111010
Njb1286 <bennionnj@gmail.com>
2022-10-05 11:03:19 -0600

    Made the labels of the inputs apear gray when they have no input

--- src/components/apps/mad-libs/input.js
+++ src/components/apps/mad-libs/input.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
-export default props => {
-    return (
-        <div className='input'>
-            <label className='input__number'>{props.index + 1}</label>
-            <input type="text" value={props.handleGetState(props.state)} name={props.state} onChange={e => props.handleInputChange(e)} />
-            <label className='input__title'>{props.title}</label>
-        </div>
-    )
+export default class Input extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            content: ""
+        }
+
+        this.handleInput = this.handleInput.bind(this);
+    }
+    
+    handleInput(content) {
+        this.setState({
+            content: content.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div className='input'>
+                <label className={`input__number${this.state.content ? ' active' : ''}`}>{this.props.index + 1}</label>
+                <input onInput={this.handleInput} type="text" value={this.props.handleGetState(this.props.state)} name={this.props.state} onChange={e => this.props.handleInputChange(e)} />
+                <label className='input__title'>{this.props.title}</label>
+            </div>
+        )
+    }
 }
