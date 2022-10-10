58c20b2c1cf35d50160a6142f869e66156c574b2
Njb1286 <bennionnj@gmail.com>
2022-10-10 11:54:59 -0600

    Placed dark skews and placed styles in multiple files for organization

--- src/components/apps/birthday-countdown/main.js
+++ src/components/apps/birthday-countdown/main.js
@@ -5,6 +5,10 @@ export default class BirthdayCountdown extends Component {
         return (
             <div className='birthday-countdown'>
                 <div className="grid">
+                    <div className="grid__skew-dark-one-box"></div>
+                    <div className="grid__skew-dark-two"></div>
+                    <div className="grid__skew-dark-three"></div>
+
                     <div className="grid__skew-light-one"></div>
                     <div className="grid__skew-light-two"></div>
                     <div className="grid__skew-light-three-box"></div>
