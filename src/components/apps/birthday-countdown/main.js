e594363a32ec08f102db5c18ff012ed6727ab35e
Njb1286 <bennionnj@gmail.com>
2022-10-10 11:38:26 -0600

    Added first set of skews

--- src/components/apps/birthday-countdown/main.js
+++ src/components/apps/birthday-countdown/main.js
@@ -4,7 +4,11 @@ export default class BirthdayCountdown extends Component {
     render() {
         return (
             <div className='birthday-countdown'>
-                
+                <div className="grid">
+                    <div className="grid__skew-light-one"></div>
+                    <div className="grid__skew-light-two"></div>
+                    <div className="grid__skew-light-three-box"></div>
+                </div>
             </div>
         )
     }
