e7adfa49858c62789ddcfdbfa1bec2a177fb1798
Njb1286 <bennionnj@gmail.com>
2022-10-11 10:50:46 -0600

    Added birthday countdown to the app

--- src/components/apps/birthday-countdown/main.js
+++ src/components/apps/birthday-countdown/main.js
@@ -5,6 +5,8 @@ export default class BirthdayCountdown extends Component {
         return (
             <div className='birthday-countdown'>
                 <div className="grid">
+                    <h1 className="grid__title">Birthday Countdown</h1>
+
                     <div className="grid__skew-dark-one-box"></div>
                     <div className="grid__skew-dark-two"></div>
                     <div className="grid__skew-dark-three"></div>
