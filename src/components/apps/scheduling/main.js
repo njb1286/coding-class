33b149c751a8ae1a24c1468b533ec800dd0aee3f
Njb1286 <bennionnj@gmail.com>
2022-10-20 10:11:01 -0600

    Bulild library and schedule components and layed them out on the grid

--- src/components/apps/scheduling/main.js
+++ src/components/apps/scheduling/main.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Library from './library/library';
+import Schedule from './schedule/schedule';
 
 export default class Scheduling extends Component {
     render() {
         return (
             <div className='scheduling'>
-                
+                <Library />
+                <Schedule />
             </div>
         )
     }
