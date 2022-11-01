3bde3ccfc3d958b7711cef50f7e223ee73547ad6
Njb1286 <bennionnj@gmail.com>
2022-11-01 10:25:13 -0600

    Added styles to the action button

--- src/components/apps/scheduling/action.js
+++ src/components/apps/scheduling/action.js
@@ -3,11 +3,30 @@ import React, { Component } from 'react';
 import { FaPlusCircle, FaTimesCircle } from 'react-icons/fa';
 
 export default class Action extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            active: false
+        }
+
+        this.handleAction = this.handleAction.bind(this);
+    }
+
+    handleAction() {
+        this.props.onClick();
+
+        this.setState({
+            active: !this.state.active
+        })
+    }
+
     render() {
         return (
-            <button onClick={this.props.onClick} className={`${this.props.className} action`}>
-                <FaPlusCircle />
-            </button>
+            <a 
+                onClick={this.handleAction} 
+                className={`${this.props.className} action${this.state.active ? " action-remove" : ""}`}
+            />
         )
     }
 }
