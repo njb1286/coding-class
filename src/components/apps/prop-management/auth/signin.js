44eeb6fb508e990413c054112ff5a53f97b69eb7
Njb1286 <bennionnj@gmail.com>
2022-11-23 11:03:33 -0700

    Revert "Added redux form"
    This reverts commit 798feb7ab5a193995696cf392a0cfae36cbf8e5c.

--- src/components/apps/prop-management/auth/signin.js
+++ src/components/apps/prop-management/auth/signin.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import SigninForm from './signin-form';
 
 export default class Signin extends Component {
     render() {
         return (
             <div className='sign-in'>
-                <SigninForm />
+                ..Signin
             </div>
         )
     }
